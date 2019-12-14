77e2bc115153ddaac13a05d4e288134bb5b70097
Tran Tuan Truong <truongsofm02@gmail.com>
2019-12-14 17:36:36 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+PROFILE_LIST = ['melting_seul', 'knhs2'] # No need to explain :D
+MULTITHREADING = False # Faster but require additional resources
+WATCH = True # Polling for new images
+WATCH_INTERVAL = 60 # How long do you want to fetch again? (in seconds)
